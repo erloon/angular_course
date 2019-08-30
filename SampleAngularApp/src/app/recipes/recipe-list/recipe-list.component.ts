35a9ed8127c187da693dd1c9a5b956291b50359d
macie <macie@DESKTOP-1EO416O>
2019-08-30 23:11:01 +0200

    recipe item

--- SampleAngularApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ SampleAngularApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('Bigos',
+  'Najlepszy pomysł na bigos',
+  'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/styles/zdjecie_glowne/public/bigos_z_kiszonej_kapusty.jpg?itok=ZeZ9_d_j')
+];
 
   constructor() { }
 
