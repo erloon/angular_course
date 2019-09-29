36a90a97a7953e04aa313a8bd46b003e07c738d6
macie <macie@DESKTOP-1EO416O>
2019-09-29 22:33:23 +0200

    Added comunication between components

--- SampleAngularApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ SampleAngularApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 recipes: Recipe[] = [
   new Recipe('Bigos',
   'Najlepszy pomysł na bigos',
+  'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/styles/zdjecie_glowne/public/bigos_z_kiszonej_kapusty.jpg?itok=ZeZ9_d_j'),
+  new Recipe('Bigos numer 2',
+  'Prawie najlepszy pomysł na bigos',
   'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/styles/zdjecie_glowne/public/bigos_z_kiszonej_kapusty.jpg?itok=ZeZ9_d_j')
 ];
 
@@ -18,4 +23,7 @@ recipes: Recipe[] = [
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
